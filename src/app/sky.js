761ac50738d0db82bc0372ddf5b52c29d3a5bc08
Dyeuse <dyeuse@gmail.com>
2022-05-11 15:47:42 -0400

    Refine Sky class

--- src/app/sky.js
+++ src/app/sky.js
@@ -1,22 +1,23 @@
 class Sky {
     constructor(zone) {
-        this.zone = zone;
-        this.sizeZone = this.zone.constructor.size;
+        this.sizeZone = zone.constructor.size;
         this.sky = document.createElement("div");
         this.sky.style.height = `${this.sizeZone.height}px`;
         this.sky.style.width = `${this.sizeZone.width}px`;
         this.sky.classList.add("sky");
-        this.zone.container.appendChild(this.sky);
-        this.zone.container.addEventListener(
-            "resizeGame",
-            this.#handleResizeGame.bind(this)
-        );
+        zone.container.appendChild(this.sky);
+        zone.container.addEventListener("resizeGame", this.#handleResizeGame);
     }
 
+    #handleResizeGame = (event) => {
+        this.sky.style.height = `${event.detail.height}px`;
+        this.sky.style.width = `${event.detail.width}px`;
+    };
+
     displayClouds(cloudsImgSrc) {
-        cloudsImgSrc.map((cloudImgSrc, velocity) =>
-            this.#createCloud(cloudImgSrc, velocity + 1)
-        );
+        cloudsImgSrc.map((cloudImgSrc, index) =>
+            this.#createCloud(cloudImgSrc, index + 1)
+        ); // index + 1 set an increasing speed for each clouds
     }
 
     #createCloud(cloudImgSrc, velocity) {
@@ -24,11 +25,6 @@ class Sky {
         cloud.style = `--velocity: ${velocity}`;
         cloud.setAttribute("src", cloudImgSrc);
         this.sky.appendChild(cloud);
-    }
-
-    #handleResizeGame(event) {
-        this.sky.style.height = `${event.detail.height}px`;
-        this.sky.style.width = `${event.detail.width}px`;
     }
 }
 
